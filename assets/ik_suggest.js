03fe2a3718642189ffa840fc47028619c660c8b0
Gordon Potter <gordon.l.potter@gmail.com>
2018-10-19 00:25:26 +0900

    Update ik_suggest.js

--- assets/ik_suggest.js
+++ assets/ik_suggest.js
@@ -2,10 +2,10 @@
  
 var pluginName = "ik_suggest",
 	defaults = {
-		'instructions': "As you start typing the application might suggest similar search terms. Use up and down arrow keys to select a suggested search string.",
 		'minLength': 2,
 		'maxResults': 10,
 		'source': []
+		
 	};
 	
 	/**
@@ -34,12 +34,7 @@ var pluginName = "ik_suggest",
 		plugin = this;
 		
 		plugin.notify = $('<div/>') // add hidden live region to be used by screen readers
-			.addClass('ik_readersonly')
-			.attr({
-				'role':'region',
-				'aria-live':'polite'
-			});
-			
+			.addClass('ik_readersonly');
 		
 		$elem = plugin.element
 			.attr({
@@ -69,7 +64,7 @@ var pluginName = "ik_suggest",
 		var plugin;
 		
 		plugin = event.data.plugin;
-		plugin.notify.text(plugin.options.instructions); 
+
 	};
 	
 	/** 
@@ -119,25 +114,7 @@ var pluginName = "ik_suggest",
 		
 		plugin = event.data.plugin;
 		$me = $(event.currentTarget);
-			switch (event.keyCode) {
-				case ik_utils.keys.down: // select next suggestion from list   
-                selected = plugin.list.find('.selected');  
-                if(selected.length) {
-                    msg = selected.removeClass('selected').next().addClass('selected').text();
-                } else {
-                    msg = plugin.list.find('li:first').addClass('selected').text();
-                }
-                plugin.notify.text(msg); // add suggestion text to live region to be read by screen reader
-                break;
-            case ik_utils.keys.up: // select previous suggestion from list
-                selected = plugin.list.find('.selected');
-                if(selected.length) {
-                    msg = selected.removeClass('selected').prev().addClass('selected').text();
-                }
-                plugin.notify.text(msg);  // add suggestion text to live region to be read by screen reader    
-                break;
-           
-            default: // get suggestions based on user input
+			
 				plugin.list.empty();
 				
 				suggestions = plugin.getSuggestions(plugin.options.source, $me.val());
@@ -152,8 +129,7 @@ var pluginName = "ik_suggest",
 				} else {
 					plugin.list.hide();
 				}
-			break;
-		}
+
 	};
 	
 	/** 
@@ -216,14 +192,10 @@ var pluginName = "ik_suggest",
 				if ( regex.test(arr[i]) ) {
 					r.push(arr[i].replace(regex, '<span>$1</span>'));
 				}
-
 			}
-		}	
-				if(r.length > 1){
-					this.notify.text('Suggestions are available for this field. Use up and down arrows to select a suggestion and enter key to use it.');
-				}
+		}
+
 		return r;
-	}
 		
 	};
 
