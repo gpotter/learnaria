a780b2aa7d0b169b04c8e00f8d649256e423902a
Gordon Potter <gordon.l.potter@gmail.com>
2018-10-20 23:01:43 -0400

    Update ik_sortable.js

--- assets/ik_sortable.js
+++ assets/ik_sortable.js
@@ -145,3 +145,100 @@ var pluginName = "ik_sortable",
 	}
  
 })( jQuery, window, document );
+				
+		plugin = event.data.plugin;
+		event = event.originalEvent || event;
+		$me = $(event.currentTarget);
+		
+		event.dataTransfer.effectAllowed = 'move';
+		event.dataTransfer.setData('text', $me.attr('id'));
+		
+	};
+	
+	Plugin.prototype.onDrop = function (event) {
+		
+		var source_id, $me;
+		
+		event = event.originalEvent || event;
+		event.preventDefault();
+		event.stopPropagation();
+		$me = $(event.currentTarget);
+		
+		source_id = event.dataTransfer.getData('text');
+		
+		if(source_id != $me.attr('id')) {
+			
+			if ($me.hasClass('dropafter')) {
+				$me.after($('#' + source_id));
+			} else {
+				$me.before($('#' + source_id));
+			}
+			
+		}
+		
+	};
+	
+	Plugin.prototype.onDragEnd = function (event) {
+		
+		var plugin;
+				
+		plugin = event.data.plugin;
+		plugin.element.find('.dragover').removeClass('dragover');
+		
+	};
+	
+	// drop target
+	
+	Plugin.prototype.onDragEnter = function (event) {
+		
+		$(event.currentTarget).addClass('dragover');
+		
+	};
+	
+	Plugin.prototype.onDragOver = function (event) {
+		
+		var $me, y, h;
+		
+		event = event.originalEvent || event;
+		event.preventDefault();
+		event.dataTransfer.dropEffect = 'move';
+		
+		$me = $(event.currentTarget);
+		
+		y = event.pageY - $me.offset().top;
+		h = $me.outerHeight();
+		
+		$me.toggleClass('dropafter', y > h / 2);
+		
+	};
+	
+	Plugin.prototype.onDragLeave = function (event) {
+		
+		$(event.currentTarget).removeClass('dragover');
+		
+	};
+	
+	Plugin.prototype.resetNumbering = function (plugin) {
+		
+		plugin.items = plugin.element.children('li');
+		
+		plugin.items.each( function(i, el) {
+			var $me = $(el);
+		});
+		
+	}
+
+	$.fn[pluginName] = function ( options ) {
+		
+		return this.each(function () {
+			
+			if ( !$.data(this, pluginName )) {
+				$.data( this, pluginName,
+				new Plugin( this, options ));
+			}
+			
+		});
+		
+	}
+ 
+})( jQuery, window, document );
