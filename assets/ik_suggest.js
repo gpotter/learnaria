d03d3a00a320371b6af597b86964c178bad239bf
Gordon Potter <gordon.l.potter@gmail.com>
2018-10-22 22:07:06 -0400

    Update ik_suggest.js

--- assets/ik_suggest.js
+++ assets/ik_suggest.js
@@ -243,3 +243,198 @@ var pluginName = "ik_suggest",
 	}
  
 })( jQuery, window, document );
+			.on('keyup', {'plugin': plugin}, plugin.onKeyUp) // add keyup event
+			.on('focusout', {'plugin': plugin}, plugin.onFocusOut);  // add focusout event
+		
+		plugin.list = $('<ul/>').addClass('suggestions');
+		
+		$elem.after(plugin.notify, plugin.list);
+				
+	};
+	
+	/** 
+	 * Handles focus event on text field.
+	 * 
+	 * @param {object} event - Keyboard event.
+	 * @param {object} event.data - Event data.
+	 * @param {object} event.data.plugin - Reference to plugin.
+	 */
+	Plugin.prototype.onFocus = function (event) {
+		
+		var plugin;
+		
+		plugin = event.data.plugin;
+		plugin.notify.text(plugin.options.instructions);
+
+	};
+	
+	/** 
+	 * Handles kedown event on text field.
+	 * 
+	 * @param {object} event - Keyboard event.
+	 * @param {object} event.data - Event data.
+	 * @param {object} event.data.plugin - Reference to plugin.
+	 */
+	Plugin.prototype.onKeyDown = function (event) {
+		
+		var plugin, selected;
+		
+		plugin = event.data.plugin;
+		
+		switch (event.keyCode) {
+					
+				case ik_utils.keys.tab:
+				case ik_utils.keys.esc:
+										
+				plugin.list.empty().hide(); // empty list and hide suggestion box
+							
+						break;
+					
+				case ik_utils.keys.enter:
+				
+				selected = plugin.list.find('.selected');
+				plugin.element.val( selected.text() ); // set text field value to the selected option
+				plugin.list.empty().hide(); // empty list and hide suggestion box
+						
+				break;
+			}
+				
+			};
+			
+			/** 
+			 * Handles keyup event on text field.
+			 * 
+			 * @param {object} event - Keyboard event.
+			 * @param {object} event.data - Event data.
+			 * @param {object} event.data.plugin - Reference to plugin.
+			 */
+			Plugin.prototype.onKeyUp = function (event) {
+				
+				var plugin, $me, suggestions, selected, msg;
+				
+				plugin = event.data.plugin;
+				$me = $(event.currentTarget);
+					
+				switch (event.keyCode) {
+					case ik_utils.keys.down: // select next suggestion from list   
+						selected = plugin.list.find('.selected');  
+						if(selected.length) {
+							msg = selected.removeClass('selected').next().addClass('selected').text();
+						} else {
+							msg = plugin.list.find('li:first').addClass('selected').text();
+						}
+						plugin.notify.text(msg); // add suggestion text to live region to be read by screen reader
+						break;
+					case ik_utils.keys.up: // select previous suggestion from list
+						selected = plugin.list.find('.selected');
+						if(selected.length) {
+							msg = selected.removeClass('selected').prev().addClass('selected').text();
+						}
+						plugin.notify.text(msg);  // add suggestion text to live region to be read by screen reader    
+						break;
+				   
+					default: // get suggestions based on user input
+			
+				plugin.list.empty();
+				
+				suggestions = plugin.getSuggestions(plugin.options.source, $me.val());
+				
+				if (suggestions.length > 1) {
+					for(var i = 0, l = suggestions.length; i < l; i++) {
+						$('<li/>').html(suggestions[i])
+						.on('click', {'plugin': plugin}, plugin.onOptionClick) // add click event handler
+						.appendTo(plugin.list);
+					}
+					plugin.list.show();
+				} else {
+					plugin.list.hide();
+				}
+			break
+		}
+	};
+	
+	/** 
+	 * Handles fosucout event on text field.
+	 * 
+	 * @param {object} event - Focus event.
+	 * @param {object} event.data - Event data.
+	 * @param {object} event.data.plugin - Reference to plugin.
+	 */
+	Plugin.prototype.onFocusOut = function (event) {
+		
+		var plugin = event.data.plugin;
+		
+		setTimeout(function() { plugin.list.empty().hide(); }, 200);
+		
+	};
+	
+	/** 
+	 * Handles click event on suggestion box list item.
+	 * 
+	 * @param {object} event - Keyboard event.
+	 * @param {object} event.data - Event data.
+	 * @param {object} event.data.plugin - Reference to plugin.
+	 */
+	Plugin.prototype.onOptionClick = function (event) {
+		
+		var plugin, $option;
+		
+		event.preventDefault();
+		event.stopPropagation();
+		
+		plugin = event.data.plugin;
+		$option = $(event.currentTarget);
+		plugin.element.val( $option.text() );
+		plugin.list.empty().hide();
+		
+	};
+	
+	/** 
+	 * Gets a list of suggestions.
+	 * 
+	 * @param {array} arr - Source array.
+	 * @param {string} str - Search string.
+	 */
+	Plugin.prototype.getSuggestions = function (arr, str) {
+		
+		var r, pattern, regex, len, limit;
+		
+		r = [];
+		pattern = '(\\b' + str + ')';
+		regex = new RegExp(pattern, 'gi');
+		len = this.options.minLength;
+		limit = this.options.maxResults;
+			
+		if (str.length >= len) {
+			for (var i = 0, l = arr.length; i < l ; i++) {
+				if (r.length > limit ) {
+					break;
+				}
+				if ( regex.test(arr[i]) ) {
+					r.push(arr[i].replace(regex, '<span>$1</span>'));
+				}
+			}
+		}
+
+		if (r.length > 1){ 	// add instructions to hidden live area
+			this.notify.text('Suggestions are available	 for this field. Use up and down arrows to select a suggestion and enter key to use it.');
+		}
+		
+		return r;
+		
+	};
+
+	$.fn[pluginName] = function ( options ) {
+		
+		return this.each(function () {
+			
+			if ( !$.data(this, pluginName )) {
+				$.data( this, pluginName,
+				new Plugin( this, options ));
+			}
+			
+		});
+		
+	}
+ 
+})( jQuery, window, document );
